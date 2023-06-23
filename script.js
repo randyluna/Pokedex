114df15ffebfdd6558bb6005b146752e7e88267a
randyluna <135148119+randyluna@users.noreply.github.com>
2023-06-23 10:29:38 -0400

    message

--- script.js
+++ script.js
@@ -27,6 +27,11 @@ const fetchPokemons= async()=>{
 }
 
 const getPokemon=async(id)=>{
-    const url=``
+    const url=`https://pokeapi.co/api/v2/pokemon/${id}`
+    const res=await fetch(url)
+    const data= await res.json();
+    console.log(data)
 }
+
+
 // fetchPokemons();
